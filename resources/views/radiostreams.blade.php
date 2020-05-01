4962142d7176362d79a243870ca99d5154e90bcf
duncte123 <duncan.sterken123@gmail.com>
2020-05-01 19:41:25 +0200

    Time to go live

--- resources/views/radiostreams.blade.php
+++ resources/views/radiostreams.blade.php
@@ -14,7 +14,7 @@
         <div class="row"></div>
         <div class="row">
             <div class="col s12">
-                <h6><strong>NOTE:</strong> This radio list is huge and search is slow atm, this is being worked on</h6>
+                <h6><strong>NOTE:</strong> This radio list is huge and search is slow atm, this is being worked on (<span id="size">Loading page...</span> radio streams in this list)</h6>
                 <input id="search_input" type="text" placeholder="Search for radio streams">
             </div>
         </div>
@@ -41,6 +41,7 @@
 @push('scripts')
     <script>
         window.searchData = @insertRadioJson;
+        id("size").innerHTML = window.searchData.length;
     </script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.9.1/underscore-min.js" crossorigin="anonymous" async defer></script>
     <script src="/js/radioSearch.js?v=3" async defer></script>
