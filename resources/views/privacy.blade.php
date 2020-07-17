fe3720005275e185ba1707db2125ec89b96ac985
duncte123 <contact@duncte123.me>
2020-07-17 22:32:32 +0200

    WIP privacy stuff

---
+++ resources/views/privacy.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.base')
+
+@section('title', 'Privacy Policy')
+
+@section('content')
+    <div class="row flow-text">
+        <div class="col s12 center">
+            <h1>Privacy Policy</h1>
+            <div>
+                stored data:
+                - guild ids
+                - text channel ids
+                - user ids
+                - reminder messages
+                - guild settings
+
+                how taken care:
+                - encrypted database
+                - deleted when leaving guilds
+            </div>
+            <div class="divider"></div>
+        </div>
+    </div>
+
+    <div class="row">
+       todo
+    </div>
+@endsection
